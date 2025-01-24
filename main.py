31269424b2bead37620d87f2f6f5ec54b223e936
Williams <carter.williams@mssm.edu>
2025-01-24 14:13:21 -0500

    Solve second half of d1
    Adds the solution for p2 of day 1

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ for i in range(len(first_list)):
     total += abs(first_list[i] - second_list[i])
 
 print(total)
+
+similarity_score = 0
+
+for i in range(len(first_list)):
+    x = first_list[i]
+    similarity_score += x * second_list.count(x)
+
+print(similarity_score)
+    
