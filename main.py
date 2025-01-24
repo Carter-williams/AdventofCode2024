6b72b42df745432da75ffdfce4297cf2bd2b1310
Williams <carter.williams@mssm.edu>
2025-01-24 14:04:05 -0500

    Solve d1
    Commit contains solution code for day one of the Advent of Code challenge.

---
+++ main.py
@@ -0,0 +1,21 @@
+input = open("C:/Users/willic130/Downloads/AOCd1input.txt", 'r')
+
+input_list = input.readlines()
+
+first_list = []
+second_list = []
+
+for line in input_list:
+    to_add = line.split()
+    first_list.append(int(to_add[0]))
+    second_list.append(int(to_add[1]))
+
+first_list.sort()
+second_list.sort()
+
+total = 0
+
+for i in range(len(first_list)):
+    total += abs(first_list[i] - second_list[i])
+
+print(total)
